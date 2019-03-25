ac452ebfbbcd698d6b148d6729139d75bc3e6882
LordAnti <30796082+LordAnti@users.noreply.github.com>
2019-03-25 15:54:38 +0300

    Клиент
    прими

--- Call of Future/Assets/Scripts/PHealth.cs
+++ Call of Future/Assets/Scripts/PHealth.cs
@@ -13,11 +13,6 @@ public class PHealth : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    private void Update()
-    {
-        
-    }
-
     public void AddDamage(float damage)
     {
         HP -= damage;
