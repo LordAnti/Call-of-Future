547b473d07fdbd00570db4c76cbd59db2ba59e4d
Artem Borisov <artem160920@mail.ru>
2019-03-25 10:56:53 +0300

    25.03.2019
    Исправлен косяк

---
+++ Call of Future/Assets/Scripts/Properties/NPCStatus.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+[CreateAssetMenu(menuName = "NPC/Status")]
+public class NPCStatus : ScriptableObject
+{
+    public bool Patrul;
+    public bool Attack;
+}
--- Call of Future/Assets/Scripts/Properties/NPSStatus.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-[CreateAssetMenu(menuName = "NPC/Status")]
-public class CharacterStatus : ScriptableObject
-{
-    public bool Patrul;
-    public bool Attack;
-}
